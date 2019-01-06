e032d83b26e8fd967b6b95d5239a4ee946efa128
svezubova <svezubova@yandex.ru>
2019-01-06 22:03:46 +0300

    c#

---
+++ hello git/hello git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет Git!");
+            Console.ReadLine();
+        }
+    }
+}
